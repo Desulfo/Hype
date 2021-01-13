1827e55e3a8d67dfa994e608506f1f5fe437df81
Pawel Janiszewski <puyol_11@interia.pl>
2021-01-13 18:57:26 +0100

    add .prettierrc

--- src/App.js
+++ src/App.js
@@ -2,38 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 
-Modal.setAppElement('#root')
+Modal.setAppElement('#root');
 function App() {
-  const [modalIsOpen,setIsOpen] = React.useState(false);
+  const [modalIsOpen, setIsOpen] = React.useState(false);
   function openModal() {
     setIsOpen(true);
   }
 
-  function closeModal(){
+  function closeModal() {
     setIsOpen(false);
   }
-return(
-  <div>
-  <button onClick={openModal}>Open Modal</button>
-  <Modal
-    isOpen={modalIsOpen}
-    onRequestClose={closeModal}
-    contentLabel="Example Modal"
-  >
-
-    <h2 >Hello</h2>
-    <button onClick={closeModal}>close</button>
-    <div>I am a modal</div>
-    <form>
-      <input />
-      <button>tab navigation</button>
-      <button>stays</button>
-      <button>inside</button>
-      <button>the modal</button>
-    </form>
-  </Modal>
-</div>
-);
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <h2>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  );
 }
 
 export default App;
